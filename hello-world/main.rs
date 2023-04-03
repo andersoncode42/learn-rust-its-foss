11f503f18e4457edcedfb51016d47e985005b062
anderson <anderson.farias@tjse.jus.br>
2023-04-03 12:31:38 -0300

    Primeira lição da série

---
+++ hello-world/main.rs
@@ -0,0 +1,20 @@
+/*
+* Fonte
+* https://itsfoss.com/rust-introduction/
+*/
+
+// Este código gera o texto
+// "Hello world!" to "stdout"
+
+fn main(){
+    println!("Hello world!");
+}
+
+/*
+
+# Compilando o programa:
+rustc main.rs
+
+# Executando o programa
+./main
+*/
